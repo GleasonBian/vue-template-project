fa704cd3d54de77b218077467ed02393c9a755c2
Gleason <bianliuzhu@gmail.com>
2020-05-03 21:19:53 +0800

    更新接口公共url

--- src/getData.js
+++ src/getData.js
@@ -1,3 +1,13 @@
+//=======================================================================
+// @Date: 2020-05-03 20:14:44
+// @LastEditTime: 2020-05-03 21:16:55
+// @Version: 7.0
+// @Author: Johnson.Wen
+// @Editor: Johnson.Wen
+// @Mobile: 13311005499
+// @Email: 13311005499@189.cn
+// @Comment: 
+//=======================================================================
 // 引入 封装请求
 import fetch from "../public/fetch.js";
 /**
@@ -8,4 +18,9 @@ import fetch from "../public/fetch.js";
 /**
  * 获取最新 消息
  */
-export const getWangYiNews = data => fetch("getWangYiNews", data, "get");
+// export const getWangYiNews = data => fetch("getWangYiNews", data, "get");
+
+/**
+ * 获取最新 消息
+ */
+export const getWangYiNews = data => fetch("corp", data, "get");
