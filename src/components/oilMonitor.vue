1cc1d594cfeedf852fb63215a837eced133fea35
Gleason <bianliuzhu@gmail.com>
2020-05-12 11:07:19 +0800

    油耗监测

--- src/components/oilMonitor.vue
+++ src/components/oilMonitor.vue
@@ -1,225 +1,431 @@
 <template>
-  <div class="amap-page-container">
-    <el-row :gutter="20">
-      <el-col :span="4">
-        <div class="grid-content bg-purple">
-          <el-table :data="tableData">
-            <el-table-column type="index" width="50" label="序号" align="center"></el-table-column>
-            <el-table-column prop="name" label="车辆名称" width="80" align="center"></el-table-column>
-            <el-table-column prop="name" label="车牌号码" width="100" align="center"></el-table-column>
-            <el-table-column label="车辆状态" width="80" align="center">
-              <template slot-scope="scope">
-                <div class="carStatus">
-                  <div
-                    class="active"
-                    :class="{'active':scope.row.status==1,'fix':scope.row.status==2,'stop':scope.row.status==3}"
-                  ></div>
-                </div>
-              </template>
-            </el-table-column>
-          </el-table>
-        </div>
-      </el-col>
-      <el-col :span="20"></el-col>
-    </el-row>
-  </div>
+  <el-row>
+    <el-col :span="4">
+      <el-table :data="vehicleData">
+        <el-table-column type="index" width="50" label="序号" align="center"></el-table-column>
+        <el-table-column prop="name" label="车辆名称" width="80" align="center"></el-table-column>
+        <el-table-column prop="name" label="车牌号码" width="100" align="center"></el-table-column>
+        <el-table-column label="车辆状态" width="80" align="center">
+          <template slot-scope="scope">
+            <div class="carStatus">
+              <div
+                class="active"
+                :class="{'active':scope.row.status==1,'fix':scope.row.status==2,'stop':scope.row.status==3}"
+              ></div>
+            </div>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-col>
+    <el-col align="middle" :span="20">
+      <!-- 搜索框 -->
+      <gt-search
+        :data="searchData"
+        @handle="assignPlans"
+        size
+        style="margin-bottom:24px"
+        ref="chart"
+      ></gt-search>
+      <!-- 列表 -->
+      <div style="width:100%; height:250px; margin-bottom:24px" ref="chart"></div>
+      <gt-table
+        :tableData="tableData"
+        style="width: 98%"
+        :optionWidth="optionWidth"
+        :columns="columns"
+        :selection="false"
+        v-on:checkTasks="checkTasks"
+        v-on:DeleteHandle="DeleteHandle"
+        v-on:UpdatePreprocessing="UpdatePreprocessing"
+        :handle="handle"
+      ></gt-table>
+      <!-- v-on:selection-change="handleSelectionChange" -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="offset"
+        :page-sizes="[10, 20, 30, 40]"
+        :page-size="10"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
+    </el-col>
+  </el-row>
 </template>
-
 <script>
-import {
-  equiSelect //设备列表
-} from "@/getData";
+import { equiSelect } from "@/getData";
 export default {
-  name: "oilMonitor",
+  name: "oilmonitor",
   data() {
     return {
-      map: null,
-      tableData: [], //设备列表
-      websock: null
+      handle: [
+        {
+          function: "checkTasks",
+          text: "查看任务",
+          type: "text",
+          show: true
+        },
+        {
+          function: "UpdatePreprocessing",
+          text: "更新计划",
+          type: "text",
+          show: true
+        },
+        {
+          function: "DeleteHandle",
+          text: "删除计划",
+          type: "text",
+          show: true
+        }
+      ],
+      columns: [
+        {
+          id: "name",
+          label: "计划名称"
+        },
+        {
+          id: "clstype",
+          label: "分类类型"
+        },
+        {
+          id: "class",
+          label: "分类类别"
+        },
+        {
+          id: "clsrank",
+          label: "分类等级"
+        },
+        {
+          id: "corpname",
+          label: "所属公司"
+        },
+        {
+          id: "deptname",
+          label: "所属部门"
+        }
+      ],
+      tableData: [], // 表格数据
+      total: 0,
+      limit: 10,
+      offset: 1,
+      multipleSelection: [], // 用于批量 删除
+      searchData: [
+        // 搜索框 数据
+        {
+          key: "proid", // 与后端交互时的字段 必填
+          label: "项目名称", // 搜索框名称 必填
+          placeholder: "请搜索", // 占位符 选填
+          default: "", // 搜索框 默认值
+          options: [
+            {
+              // 选填 如果 存在 options 选项 搜索框将由 input 变为 select框
+              value: "项目1", // 下拉选项 绑定 值
+              label: "项目1" // 下拉选项 绑定 名称
+            },
+            {
+              value: "项目2",
+              label: "项目2"
+            }
+          ]
+        },
+        {
+          key: "time",
+          label: "近期时间",
+          placeholder: "选择近期时间",
+          default: "",
+          options: [
+            {
+              // 选填 如果 存在 options 选项 搜索框将由 input 变为 select框
+              value: "今天", // 下拉选项 绑定 值
+              label: "今天" // 下拉选项 绑定 名称
+            },
+            {
+              value: "昨天",
+              label: "昨天"
+            },
+            {
+              value: "近7天",
+              label: "近7天"
+            },
+            {
+              value: "近30天",
+              label: "近30天"
+            }
+          ]
+        },
+        {
+          key: "date",
+          label: "选择时间",
+          placeholder: "请选择",
+          default: ""
+        }
+      ],
+      optionWidth: 250,
+      vehicleData: []
     };
   },
-  components: {},
-  computed: {},
-  created() {},
-  destroyed() {
-    this.websock.close(); //离开路由之后断开websocket连接
+  beforeCreate() {},
+  created() {
+    this.equiList();
   },
   mounted() {
-    this.equiList();
+    this.initCharts();
   },
   methods: {
     async equiList() {
-      //设备列表
       const res = await equiSelect();
-      this.tableData = res.data;
+      this.vehicleData = res.data;
     },
-
-    async initCharts() {
-      const res = await allCollect();
-
-      let amount = [],
-        oil = [],
-        time = [],
-        xAxis = [];
-
-      res.data.forEach(function(element) {
-        xAxis.push(element.equipname);
-        amount.push(element.totalamout);
-        oil.push(element.totaloil);
-        time.push(element.totaltime);
-      });
-
-      this.total = {
-        xAxis: xAxis,
-        yAxis: {
-          oil,
-          amount,
-          time
-        }
-      };
-
+    initCharts() {
+      let colors = ["#5793f3", "#d14a61", "#675bba"];
       let myChart = this.$echarts.init(this.$refs.chart);
-      myChart.setOption({
-        title: {
-          text: "油耗产出总览",
-          textStyle: {
-            // color: "#",
-          },
-          left: "center"
-        },
+      let option = {
+        color: colors,
         tooltip: {
           trigger: "axis",
           axisPointer: {
-            type: "cross",
-            crossStyle: {
-              color: "#999"
-            }
+            type: "cross"
+          }
+        },
+        grid: {
+          right: "20%"
+        },
+        toolbox: {
+          feature: {
+            dataView: { show: true, readOnly: false },
+            restore: { show: true },
+            saveAsImage: { show: true }
           }
         },
         legend: {
-          data: ["总油量", "总耗时", "总产出"],
-          top: "10%" //与上方的距离 可百分比% 可像素px
+          data: ["蒸发量", "降水量", "平均温度"]
         },
         xAxis: [
           {
             type: "category",
-            data: this.total.xAxis,
-            axisPointer: {
-              type: "shadow"
-            }
+            axisTick: {
+              alignWithLabel: true
+            },
+            data: [
+              "1月",
+              "2月",
+              "3月",
+              "4月",
+              "5月",
+              "6月",
+              "7月",
+              "8月",
+              "9月",
+              "10月",
+              "11月",
+              "12月"
+            ]
           }
         ],
         yAxis: [
           {
             type: "value",
-            name: "升",
+            name: "蒸发量",
             min: 0,
-            interval: 50,
+            max: 250,
+            position: "right",
+            axisLine: {
+              lineStyle: {
+                color: colors[0]
+              }
+            },
             axisLabel: {
-              formatter: "{value} L"
+              formatter: "{value} ml"
             }
           },
           {
             type: "value",
-            name: "小时",
+            name: "降水量",
             min: 0,
-            interval: 5,
+            max: 250,
+            position: "right",
+            offset: 80,
+            axisLine: {
+              lineStyle: {
+                color: colors[1]
+              }
+            },
             axisLabel: {
-              formatter: "{value} H"
+              formatter: "{value} ml"
+            }
+          },
+          {
+            type: "value",
+            name: "温度",
+            min: 0,
+            max: 25,
+            position: "left",
+            axisLine: {
+              lineStyle: {
+                color: colors[2]
+              }
+            },
+            axisLabel: {
+              formatter: "{value} °C"
             }
           }
         ],
         series: [
           {
-            name: "总油量",
+            name: "蒸发量",
             type: "bar",
-            data: this.total.yAxis.oil
+            data: [
+              2.0,
+              4.9,
+              7.0,
+              23.2,
+              25.6,
+              76.7,
+              135.6,
+              162.2,
+              32.6,
+              20.0,
+              6.4,
+              3.3
+            ]
           },
           {
-            name: "总耗时",
+            name: "降水量",
             type: "bar",
-            data: this.total.yAxis.time
-          },
-          {
-            name: "总产出",
-            type: "line",
             yAxisIndex: 1,
-            data: this.total.yAxis.amount
+            data: [
+              2.6,
+              5.9,
+              9.0,
+              26.4,
+              28.7,
+              70.7,
+              175.6,
+              182.2,
+              48.7,
+              18.8,
+              6.0,
+              2.3
+            ]
+          },
+          {
+            name: "平均温度",
+            type: "line",
+            yAxisIndex: 2,
+            data: [
+              2.0,
+              2.2,
+              3.3,
+              4.5,
+              6.3,
+              10.2,
+              20.3,
+              23.4,
+              23.0,
+              16.5,
+              12.0,
+              6.2
+            ]
           }
         ]
-      });
+      };
+      myChart.setOption(option);
       window.onresize = function() {
         myChart.resize();
       };
     },
+    /*
+     ** 查看处理
+     */
+    assignPlans() {},
 
-    initWebSocket() {
-      //初始化weosocket
-      const wsuri = "ws://192.168.3.210:8090/ws/leffss";
-      // 建立连接
-      this.websock = new WebSocket(wsuri);
-      this.websock.onmessage = this.websocketonmessage;
-      this.websock.onopen = this.websocketonopen;
-      this.websock.onerror = this.websocketonerror;
-      this.websock.onclose = this.websocketclose;
+    /**
+     * 查看任务列表
+     * **/
+    async checkTasks(index, row) {},
+
+    /*
+     ** 查看处理
+     */
+    async ExamineHandle(index, row) {},
+
+    /*
+     ** 更新预处理
+     */
+    async UpdatePreprocessing(index, row) {},
+
+    /*
+     ** 更新处理
+     */
+    async UpdateHandle(index, row) {},
+
+    /*
+     ** 删除处理
+     */
+    async DeleteHandle(index, row) {
+      this.$confirm("删除计划?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          let res = assignDelete({ id: row.guid });
+          console.log(res);
+          if (res.status === 200) {
+            this.$message.success("删除成功");
+          }
+          this.assignPlans();
+        })
+        .catch(err => {});
     },
-    websocketonmessage(event) {
-      //数据接收
-      if (event.data instanceof Blob) {
-        let reader = new FileReader();
 
-        reader.onload = () => {
-          if (reader.result == "Hello WebSockets!") return;
-          let data = reader.result;
-          data = eval("(" + data + ")");
-          console.log("解析->", data);
-          this.center = [data.longitude, data.latitude];
-          let obj = { position: [data.longitude, data.latitude] };
-          this.markers.push(obj);
-          this.map.markers(this.markers);
-          /* 
-            altitude: 12.345                                  // 海拔高度
-            curmiles: 12                                      // 当前里程 = 今日里程
-            curoilconsume: 5.3                                // 当前油耗
-            curoilpos: 9.3                                    // 当前油位
-            direction: 12                                     // 行驶方向
-            gpstime: "2020-05-10 16:30:30"                    // gps 时间
-            guid: "e7d52b53-2307-4b09-aec8-f61d6cea6cf5"      // guid
-            latitude: 36.840376                               // 纬度
-            longitude: 112.860237                             // 经度
-            name: "机车一号"                                   // 机车名称
-            servertime: "2020-05-10 16:30:30"                 // 服务器时间
-            speed: 60                                         // 速度
-            totalmiles: 138                                   // 总里程
-          */
-        };
-
-        reader.readAsText(event.data);
-      } else {
-        console.log("Result2: " + event.data);
+    /*
+     ** 列表 批量删除 用户
+     */
+    async BatchDeleteUser() {
+      if (this.multipleSelection.length === 0) {
+        this.$message.warning("请选择删除数据!");
+        return;
       }
+      let data = {
+        ids: JSON.stringify(this.multipleSelection)
+      };
+      let res = await deleteUserByIds(data);
+      if (res.result) this.$message.success(res.message);
+      else this.$message.warning(res.message);
+      this.$refs.searchBox.internalUser(this.limit, this.offset);
     },
-    websocketonopen() {
-      //连接建立之后执行send方法发送数据
-      this.websocketsend("Hello WebSockets!");
+
+    /*
+     ** 列表 批量删除 用户  预处理
+     */
+    handleSelectionChange(val) {
+      let arr = [];
+      for (var item of val) arr.push(item.id);
+      this.multipleSelection = arr;
     },
-    websocketsend(Data) {
-      //数据发送
-      this.websock.send(Data);
+
+    /*
+     ** 分页处理
+     */
+    handleSizeChange(val) {
+      let arr = [];
+      for (var item of val) arr.push(item.id);
+      this.multipleSelection = arr;
     },
-    websocketonerror() {
-      //连接建立失败重连
-      this.initWebSocket();
-    },
-    websocketclose(e) {
-      //关闭
-      console.log("断开连接", e);
+
+    /*
+     ** 分页处理2
+     */
+    handleCurrentChange(val) {
+      let arr = [];
+      for (var item of val) arr.push(item.id);
+      this.multipleSelection = arr;
     }
   },
-  updated() {}
+  components: {}
 };
 </script>
-
 <style lang="less" scoped>
 @red: #ff0000;
 @green: #66cc66;
@@ -241,51 +447,5 @@ export default {
   .stop {
     background: @gray;
   }
-}
-.carList {
-  width: 30%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 100;
-}
-.amap-page-container {
-  width: 100%;
-  height: 100%;
-}
-.map {
-  height: 90vh;
-}
-.amap-demo {
-  height: 93vh;
-}
-.card {
-  position: absolute;
-  top: 0px;
-  right: 0px;
-  z-index: 100;
-}
-
-.clearfix:before,
-.clearfix:after {
-  display: table;
-  content: "";
-}
-.clearfix {
-  border-bottom: 1px solid #00000054;
-}
-.clearfix:after {
-  clear: both;
-}
-.card_content {
-  display: inline-block;
-  height: 40px;
-  line-height: 40px;
-  font-size: 20px;
-  font-weight: bold;
-}
-.box-card {
-  width: 300px;
-  margin-bottom: 20px;
 }
 </style>
