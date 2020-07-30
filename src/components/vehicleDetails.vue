6e379407f2b385945a11a0bbd4475d802f4656d0
Gleason <bianliuzhu@gmail.com>
2020-07-30 15:30:40 +0800

    添加车辆详情

---
+++ src/components/vehicleDetails.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+   
+  </div>
+</template>
+<script>
+import {  } from "@/getData";
+export default {
+  name: "alarm",
+  data() {
+    return {
+      
+    };
+  },
+  created() {
+   
+  },
+  methods: {
+    /**
+     * 告警信息
+     */
+    async alarmHandle() {
+      
+    },
+   
+  },
+  watch: {
+   
+  }
+};
+</script>
+<style>
+
+</style>
